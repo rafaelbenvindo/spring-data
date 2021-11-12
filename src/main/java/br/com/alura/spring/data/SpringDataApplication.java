197bb92d11dc8e69e1763acbbb7645a72eea970d
Rafael Benvindo <rafael.benvindo@trt1.jus.br>
2021-11-12 04:30:45 -0300

    Finaliza a aula 1

---
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -0,0 +1,45 @@
+package br.com.alura.spring.data;
+
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.spring.data.service.CrudCargoService;
+
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner {
+
+	private Boolean system = true;
+
+	private final CrudCargoService cargoService;
+
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+
+		while (system) {
+			System.out.println("Digite a opção desejada:");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+
+			int action = scanner.nextInt();
+			if (action == 1) {
+				cargoService.inicial(scanner);
+			} else {
+				system = false;
+			}
+		}
+
+	}
+
+}
